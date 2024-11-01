3d8db217ba6cbfd231d67a4fdfc7bd3849180561
Bastian Spanneberg <bastian@spanneberg.com>
2024-11-01 22:14:55 +0100

    adjusted eksctl config to include ebs-csi. added folder for steadybit deployment

--- terraform/mgmt-cluster/gateway.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "conli24_internet_gateway" {
-  vpc_id = aws_vpc.conli24_vpc.id
-
-  tags = {
-    Name = "conli24-internet-gateway"
-  }
-}
--- terraform/mgmt-cluster/vpc.tf
+++
@@ -1,8 +0,0 @@
-# VPC for all subnets
-resource "aws_vpc" "conli24_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "conli24-vpc"
-  }
-}
